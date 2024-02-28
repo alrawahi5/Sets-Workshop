9b1d8a26ab15f5370dea649e93c8e4f62458635f
ABDULLAH <abdullah555523@hotmail.com>
2024-02-28 09:32:43 +0400

    added the MV unique element

--- SetWorkshopMVtheNotExsist.java
+++ SetWorkshopMVtheNotExsist.java
@@ -6,10 +6,10 @@ public class SetWorkshopMVtheNotExsist{
             Set<Integer> num1 = new HashSet<Integer>();
             Set<Integer> num2 = new HashSet<Integer>();
             num1.addAll(Arrays.asList(1, 2, 3));
-            num2.addAll(Arrays.asList(1, 4, 5));
+            num2.addAll(Arrays.asList(1, 2, 3,4));
+            System.out.println("The first set contains: " + num1);
+            System.out.println("The second set contains: "+ num2);
             boolean a = num1.retainAll(num2);
-            System.out.println("Intesection element "+num1);
-            num1.clear();
-            System.out.println(num1);
+            System.out.println("After removing the unique element\n in the second set, we get: "+num1);
         }
     }
